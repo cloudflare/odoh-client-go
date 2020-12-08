936ca4de9e9ac06ef7df771b3145cd1f9564ca47
Feroz Salam <feroz@zaytoun.co>
2020-12-08 21:43:53 +0000

    Change the default PROXY_HTTP_MODE to https
    As per the protocol diagram on https://blog.cloudflare.com/oblivious-dns/,
    the communication from the client to the proxy is encrypted using HTTPS.
    When I was adding ODoH proxying support to my DoH service, I attempted
    to test the connection and did not expect the default to be set to HTTP.
    I think it would be more intuitive for the default to be HTTPS, as that
    is the mode indicated in the diagram, and hopefully the mode which most people
    will be using proxies.

--- commands/common.go
+++ commands/common.go
@@ -4,6 +4,6 @@ const (
 	DEFAULT_DOH_SERVER        = "cloudflare-dns.com"
 	OBLIVIOUS_DOH             = "application/oblivious-dns-message"
 	TARGET_HTTP_MODE          = "https"
-	PROXY_HTTP_MODE           = "http"
+	PROXY_HTTP_MODE           = "https"
 	ODOH_CONFIG_WELLKNOWN_URL = "/.well-known/odohconfigs"
 )
